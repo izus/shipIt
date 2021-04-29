2b8276655f065d0ab714a71f24af6a71e714b7df
izus <admin@izus.com.ar>
2021-04-29 17:06:56 -0300

    Fixing quotation functions

--- src/ShipIt.php
+++ src/ShipIt.php
@@ -26,7 +26,7 @@ class ShipIt
 
     protected $email = null;
     protected $token = null;
-    protected $apiBase = 'http://api.shipit.cl/v/';
+    protected $apiBase = 'https://api.shipit.cl/v/';
 
     protected $environment = 'production';
 
@@ -44,6 +44,325 @@ class ShipIt
     ];
 
     /**
+     * Constructor de la clase
+     *
+     * @param null $email email de la cuenta
+     * @param null $token token de acceso
+     */
+    public function __construct($email = null, $token = null, $environment = 'production')
+    {
+        $this->email($email);
+        $this->token($token);
+
+        if (!in_array($environment, [ShipIt::ENV_DEVELOPMENT, ShipIt::ENV_PRODUCTION])) {
+            $environment = ShipIt::ENV_PRODUCTION;
+        }
+
+        $this->environment($environment);
+
+    }
+
+    /**
+     * Retorna el email registrado. Si se define un valor se sobre-escribe el valor actual.
+     *
+     * @param null $email email de la cuenta
+     * @return string
+     */
+    function email($email = null)
+    {
+        if ($email !== null) {
+            $this->email = $email;
+        }
+
+        return $this->email;
+    }
+
+
+    /**
+     * Retorna el token de acceso. Si se define un valor se sobre-escribe el valor actual.
+     *
+     * @param null $token token de acceso
+     * @return string
+     */
+    function token($token = null)
+    {
+        if ($token !== null) {
+            $this->token = $token;
+        }
+
+        return $this->token;
+    }
+
+    /**
+     * Retorna el environment. Si se define un valor se sobre-escribe el valor actual.
+     *
+     * @param null $environment ambiente
+     * @return string
+     */
+    function environment($environment = null)
+    {
+        if ($environment !== null) {
+            $this->environment = $environment;
+        }
+
+        return $this->environment;
+    }
+
+    /**
+     * Retorna un array con las regiones disponibles en ShipIt
+     *
+     * @return array
+     */
+    public function getRegions($asArray = false)
+    {
+        $regions = $this->get(self::METHOD_GET, '/regions');
+        return $regions;
+    }
+
+
+    /**
+     * Retorna la respuesta de un endpoint
+     *
+     * @param string $method método del endpoint
+     * @param string $endpoint endpoint a consultar
+     * @param array $data datos a enviar al endpoint
+     * @throws TokenNotFoundException
+     * @throws EmailNotFoundException
+     * @throws ConnectException
+     * @return object
+     */
+    public function get($method, $endpoint, array $data = null, $version = 'v2')
+    {
+        if ($this->token === null) {
+            throw new TokenNotFoundException();
+        }
+
+        if ($this->email === null) {
+            throw new EmailNotFoundException();
+        }
+
+        $endpoint = (strpos($endpoint, '/') == 0) ? substr($endpoint, 1) : $endpoint;
+        $endpoint = $this->apiBase . $endpoint;
+
+        try {
+            $config = [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'X-Shipit-Email' => $this->email,
+                    'X-Shipit-Access-Token' => $this->token,
+                    'Accept' => "application/vnd.shipit.$version"
+                ]
+            ];
+
+            if (in_array($method, array(self::METHOD_POST, self::METHOD_PUT))) {
+                $config['json'] = $data;
+            }
+
+
+            $client = new \GuzzleHttp\Client();
+            $res = $client->request($method, $endpoint, $config);
+
+            if ($res->getStatusCode() == 404) {
+                throw new EndpointNotFoundException($endpoint);
+            }
+
+            $response = json_decode($res->getBody());
+        } catch (GuzzleConnectException $e) {
+            throw new ConnectException($endpoint);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Retorna un array con las comunas disponibles en ShipIt
+     *
+     * @return array
+     */
+    public function getCommunes($asArray = false)
+    {
+        $communes = $this->get(self::METHOD_GET, '/communes');
+        return $communes;
+    }
+
+    /**
+     * Envia una solicitud de delivery
+     *
+     * @param ShippingRequest $request
+     * @return ShippingRequestResponse
+     */
+    public function requestShipping(ShippingRequest $request)
+    {
+        $data = [
+            'package' => $request->toShipItFormat($this->environment())
+        ];
+
+        $response = $this->get(self::METHOD_POST, '/packages', $data);
+
+        return new ShippingRequestResponse($response);
+    }
+
+    /**
+     * Envia un solicitud de delivery para multiples items
+     *
+     * @param array $items arreglo de ShippingRequest's
+     * @return ShippingRequestResponse
+     */
+    public function requestMassiveShipping(array $items)
+    {
+        $data = [
+            'packages' => []
+        ];
+
+        foreach ($items as $item) {
+            $data['packages'][] = $item->toShipItFormat($this->environment());
+        }
+
+        $response = $this->get(self::METHOD_POST, '/packages/mass_create', $data);
+
+        return new ShippingRequestResponse($response);
+    }
+
+
+    /**
+     * Retorna el historial de despachos para una fecha
+     *
+     * @param null $date
+     * @return ShippingHistoryResponse
+     */
+    public function getAllShippings($date = null)
+    {
+        if (is_null($date)) {
+            $date = Carbon::today();
+        }
+
+        $date = (gettype($date) == 'object') ? Carbon::parse($date->format('Y-m-d')) : Carbon::parse($date);
+
+        $params = [
+            "year={$date->year}",
+            "month={$date->month}",
+            "day={$date->day}"
+        ];
+
+        $response = $this->get(self::METHOD_GET, '/packages?' . implode('&', $params));
+
+        return new ShippingHistoryResponse($response);
+    }
+
+
+    /**
+     * Retorna el detalle de un despacho
+     *
+     * @param $id
+     * @return Shipping
+     * @throws NumberNotValidException
+     */
+    public function getShipping($id)
+    {
+        if (!is_numeric($id)) {
+            throw new NumberNotValidException($id);
+        }
+
+        $response = $this->get(self::METHOD_GET, '/packages/' . $id);
+
+        return new Shipping($response, $this);
+    }
+
+    /**
+     * Retorna el tamaño aproximado del paquete
+     *
+     * @param int $width ancho en cm.
+     * @param int $height alto en cm.
+     * @param int $length longitud en cm (profundidad)
+     * @return string|null
+     */
+    public function getPackageSize($width, $height, $length)
+    {
+        if (!is_numeric($width) or !is_numeric($height) or !is_numeric($length)) {
+            return null;
+        }
+
+        $packageSize = 0;
+        $packageSize = ($packageSize < $height) ? $height : $packageSize;
+        $packageSize = ($packageSize < $width) ? $width : $packageSize;
+        $packageSize = ($packageSize < $length) ? $length : $packageSize;
+
+        foreach ($this->packageSizes as $sizeLimit => $name) {
+            if ($packageSize <= $sizeLimit) {
+                return $name;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Recibe el callback entregado por ShipIt y lanza el evento que corresponda (solo Laravel)
+     */
+    public function receiveCallback()
+    {
+        $data = request()->all();
+        $method = request()->method();
+
+        if ($method == 'POST') {
+            event(new ShipItCallbackPostEvent($data));
+        }
+
+        if (in_array($method, ['PUT', 'PATCH'])) {
+            event(new ShipItCallbackPutEvent($data));
+        }
+
+        return response('ok');
+
+    }
+
+    /**
+     * Retorna un listado de cotizaciones
+     *
+     * @param QuotationRequest $request
+     * @return Quotation
+     */
+    public function getQuotation($data)
+    {
+        $quotation  = $this->get(self::METHOD_POST, '/prices', $data, 'v3');
+        return $quotation;
+    }
+
+    /**
+     * Retorna la cotización mas rápida y económica
+     *
+     * @param QuotationRequest $request
+     * @return QuotationItem
+     */
+    public function getBestQuotation($data)
+    {
+        $response = $this->get(self::METHOD_POST, '/prices', $data, 'v3');
+
+        if($response->lower_price){
+            return $response->lower_price;
+        }
+        return [];
+    }
+
+    /**
+     * Retorna la url de seguimiento de un envio
+     *
+     * @param string $provider proveedor de servicio
+     * @param string $trackingNumber número de tracking
+     * @return string
+     */
+    public function getTrackingUrl($provider, $trackingNumber)
+    {
+        if (!isset($this->providersTrakingUrls[$provider])) {
+            return null;
+        }
+
+        $url = $this->providersTrakingUrls[$provider];
+
+        return str_replace(":number", $trackingNumber, $url);
+    }
+}
+
      * Constructor de la clase
      *
      * @param null $email email de la cuenta
